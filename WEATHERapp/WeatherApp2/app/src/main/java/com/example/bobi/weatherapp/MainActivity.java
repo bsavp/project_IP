c74d41666cf421683669bbd95e40742f0511a545
Боряна Савова <hihihu@abv.bg>
2017-05-08 15:34:40 +0300

    I give up on this

--- WEATHERapp/WeatherApp2/app/src/main/java/com/example/bobi/weatherapp/MainActivity.java
+++ WEATHERapp/WeatherApp2/app/src/main/java/com/example/bobi/weatherapp/MainActivity.java
@@ -1,16 +1,79 @@
 package com.example.bobi.weatherapp;
 
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import data.JSONWeatherParser;
+import data.WeatherHttpClient;
+import model.Weather;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView cityName;
+    private TextView temp;
+    private ImageView iconView;
+    private TextView description;
+    private TextView humidity;
+    private TextView pressure;
+    private TextView wind;
+    private TextView updated;
 
 
+    Weather weather = new Weather();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityName = (TextView) findViewById(R.id.cityText);
+        iconView = (ImageView) findViewById(R.id.thumbnailIcon);
+        temp = (TextView) findViewById(R.id.tempText);
+        description = (TextView) findViewById(R.id.cloudText);
+        humidity = (TextView) findViewById(R.id.humidText);
+        pressure = (TextView) findViewById(R.id.pressureText);
+        wind = (TextView) findViewById(R.id.windText);
+        updated = (TextView) findViewById(R.id.lastUpadtedText);
+
+        renderWeatherData("Sofia,BG");
     }
+
+    public void renderWeatherData(String city){
+
+    WeatherTask weatherTask = new WeatherTask();
+        weatherTask.execute(new String[]{city + "&units=metric"});
+
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, Weather>{
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+
+
+
+
+
+            super.onPostExecute(weather);
+        }
+
+        @Override
+        protected Weather doInBackground(String... params) {
+
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            weather = JSONWeatherParser.getWeather(data);
+
+            Log.v("Data: ", weather.place.getCity());
+
+            return weather;
+        }
+    }
+
+
 }
